19ed550f57ec76371cd63010d6688c0d0c28142b
Dennis339 <dennischacon8@gmail.com>
2026-04-25 15:10:57 -0400

    Finalización del ciclo de juego
    Se estructuró la clase POO y el flujo del bucle.

--- ejemplo.py
+++ ejemplo.py
@@ -1,62 +1,46 @@
-# nombre = input("¿Cuál es tu nombre? ")
-# print("¡Hola, " + nombre + "! Bienvenido al mundo de la programación en Python.")
-# print(f"Bienvenido al mundo de la programación en Python, {nombre}!")
-# dinero = 100
-# dignidad = 50
-# hambre = 0
-# print("El jugador ha recibido su herencia.")
-
-# print("Que desea hacer el jugador?")
-# print("1. Gastar dinero en fiestas")
-# print("2. Invertir una parte")
-# print("3. Ahorrar")
-
-# opcion = input("Ingrese el número de la opción que desea elegir: ")
-# if opcion == "1":
-#     dinero -= 20
-#     dignidad -= 10
-#     hambre += 5
-#     print("El jugador ha gastado dinero en fiestas. Dinero:", dinero, "Dignidad:", dignidad, "Hambre:", hambre)
-# elif opcion == "2":
-#     dinero -= 30
-#     dignidad += 5
-#     hambre += 2
-#     print("El jugador ha invertido una parte de su dinero. Dinero:", dinero, "Dignidad:", dignidad, "Hambre:", hambre)
-# elif opcion == "3":
-#     dinero += 10
-#     dignidad += 2
-#     hambre -= 3
-#     print("El jugador ha ahorrado. Dinero:", dinero, "Dignidad:", dignidad, "Hambre:", hambre)
-# else:
-#     print("Opción no válida.")
-    
 class HijoProdigo:
     def __init__(self, nombre):
         self.nombre = nombre
         self.dinero = 100
         self.dignidad = 50
         self.hambre = 0
-        self.arrepentimiento = 0
-
-    def gastar_en_fiestas(self):
-        self.dinero -= 20
-        self.dignidad -= 10
-        self.hambre += 5
-        print(f"{self.nombre} ha gastado dinero en fiestas. Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
-
-    def invertir(self):
-        self.dinero -= 30
-        self.dignidad += 5
-        self.hambre += 2
-        print(f"{self.nombre} ha invertido una parte de su dinero. Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
-
-    def ahorrar(self):
-        self.dinero += 10
-        self.dignidad += 2
-        self.hambre -= 3
-        print(f"{self.nombre} ha ahorrado. Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
         
-opcion2 = input("Ingrese el número de la opción que desea elegir: ")
-if opcion2 == "1":    
-    hijo = HijoProdigo("AA")
-    hijo.gastar_en_fiestas()
+    def gastar_todo(self):
+        self.dinero = 0
+        self.dignidad -= 20
+        self.hambre += 50
+        print(f"\n¡Derroche total! {self.nombre} se quedó sin dinero.")
+        print(f"Dignidad: {self.dignidad} | Hambre: {self.hambre}")
+
+    def invertir_o_trabajar(self):
+        self.dinero += 20
+        self.dignidad += 5 
+        self.hambre += 10
+        print(f"\n{self.nombre} trabajó e invirtió con éxito.")
+        print(f"Ganaste 20 monedas. (Dignidad +5, Hambre +10)")
+# --- INICIO ---
+nombre_jugador = input("Ingrese el nombre del jugador: ")
+hijo = HijoProdigo(nombre_jugador)
+
+print(f"\nBienvenido {hijo.nombre}. Recibes tu herencia de 100 monedas.")
+
+while hijo.dinero > 0:
+    print(f"\n--- ESTADO: Dinero {hijo.dinero} | Dignidad {hijo.dignidad} | Hambre {hijo.hambre} ---")
+    print("1. Gastarlo todo en fiestas")
+    print("2. Invertir / Trabajar")
+    print("3. Ahorrar")
+    
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        hijo.gastar_todo()
+    elif opcion == "2":
+        hijo.invertir_o_trabajar()
+    elif opcion == "3":
+        print("Decidiste ahorrar.")
+    
+    # REQUISITO: El costo de vida que intenta frenar el bucle
+    print("“Sigues viviendo lejos de casa…”")
+    hijo.dinero -= 10 
+
+print(f"\nEl dinero de {hijo.nombre} llegó a 0. Regresas con tu padre.")
