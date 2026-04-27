6e6f67238b85e9bf4b893b0401ed79b18e8dbbed
MarvinChara <marvin.chara@uab.edu.bo>
2026-04-26 21:19:47 -0400

    Mejora: Implementación de clase HijoProdigo con menú interactivo y estadísticas

--- ejemplo.py
+++ ejemplo.py
@@ -1,62 +1,59 @@
-# nombre = input("¿Cuál es tu nombre? ")
-# print("¡Hola, " + nombre + "! Bienvenido al mundo de la programación en Python.")
-# print(f"Bienvenido al mundo de la programación en Python, {nombre}!")
-# dinero = 100
-# dignidad = 50
-# hambre = 0
-# print("El jugador ha recibido su herencia.")
-
-# print("Que desea hacer el jugador?")
-# print("1. Gastar dinero en fiestas")
-# print("2. Invertir una parte")
-# print("3. Ahorrar")
-
-# opcion = input("Ingrese el número de la opción que desea elegir: ")
-# if opcion == "1":
-#     dinero -= 20
-#     dignidad -= 10
-#     hambre += 5
-#     print("El jugador ha gastado dinero en fiestas. Dinero:", dinero, "Dignidad:", dignidad, "Hambre:", hambre)
-# elif opcion == "2":
-#     dinero -= 30
-#     dignidad += 5
-#     hambre += 2
-#     print("El jugador ha invertido una parte de su dinero. Dinero:", dinero, "Dignidad:", dignidad, "Hambre:", hambre)
-# elif opcion == "3":
-#     dinero += 10
-#     dignidad += 2
-#     hambre -= 3
-#     print("El jugador ha ahorrado. Dinero:", dinero, "Dignidad:", dignidad, "Hambre:", hambre)
-# else:
-#     print("Opción no válida.")
-    
 class HijoProdigo:
     def __init__(self, nombre):
         self.nombre = nombre
         self.dinero = 100
         self.dignidad = 50
         self.hambre = 0
-        self.arrepentimiento = 0
+
+    def mostrar_estado(self):
+        """Muestra las estadísticas actuales del jugador."""
+        print(f"\n---  ESTADO DE {self.nombre.upper()} ---")
+        print(f" Dinero: {self.dinero}")
+        print(f" Dignidad: {self.dignidad}")
+        print(f" Hambre: {self.hambre}")
+        print("-" * 30)
 
     def gastar_en_fiestas(self):
         self.dinero -= 20
         self.dignidad -= 10
         self.hambre += 5
-        print(f"{self.nombre} ha gastado dinero en fiestas. Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
+        print(f"\n {self.nombre} se fue de fiesta. ¡La noche estuvo loca!")
 
     def invertir(self):
         self.dinero -= 30
         self.dignidad += 5
         self.hambre += 2
-        print(f"{self.nombre} ha invertido una parte de su dinero. Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
+        print(f"\n {self.nombre} invirtió en criptomonedas. ¡Mentalidad de tiburón!")
 
     def ahorrar(self):
         self.dinero += 10
         self.dignidad += 2
         self.hambre -= 3
-        print(f"{self.nombre} ha ahorrado. Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
-        
-opcion2 = input("Ingrese el número de la opción que desea elegir: ")
-if opcion2 == "1":    
-    hijo = HijoProdigo("AA")
-    hijo.gastar_en_fiestas()
+        print(f"\n {self.nombre} guardó dinero bajo el colchón.")
+
+# --- Lógica de ejecución ---
+if __name__ == "__main__":
+    user_name = input("Ingrese un nombre de jugador")
+    hijo = HijoProdigo(user_name)
+
+    while True:
+        hijo.mostrar_estado()
+        print("¿Qué desea hacer el jugador?")
+        print("1. Gastar dinero en fiestas")
+        print("2. Invertir una parte")
+        print("3. Ahorrar")
+        print("4. Salir del programa")
+
+        opcion = input("\nSeleccione una opción (1-4): ")
+
+        if opcion == "1":
+            hijo.gastar_en_fiestas()
+        elif opcion == "2":
+            hijo.invertir()
+        elif opcion == "3":
+            hijo.ahorrar()
+        elif opcion == "4":
+            print("¡Cerrando el simulador!, gracias por jugar.")
+            break
+        else:
+            print("Opción no válida, intente de nuevo.")
